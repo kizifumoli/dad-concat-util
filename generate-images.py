464614b0c8ef64e8a190f097acf28e192f261a7d
kizi fumoli <kizi.fumoli23@gmail.com>
2024-02-24 22:51:09 -0800

    [concat] Add auto-resizing functionalty
    Users can no resize images by passing in
    
    ````
    --resize-widths --resize-heights
    ````
    
    Or force the script to not resize images by
    
    ````
    --no-resize-widths --no-resize-heights
    ````
    
    By default, if the user provides neither of these sets of options, the
    script assumes the user wants to resize the images

--- generate-images.py
+++ generate-images.py
@@ -9,7 +9,7 @@ from pathlib import Path
 def gen_random_color():
     return (random.randint(175, 255), random.randint(175, 255), random.randint(175, 255))
 
-def create_numbered_images(n_colors: int, output_dir: Path):
+def create_numbered_images(n_colors: int, output_dir: Path, vary_widths=True, vary_heights=True):
     text_font = ImageFont.truetype(
         str(Path.home() / ".local/share/fonts/FiraCode-Regular.ttf"),
         50)
@@ -18,41 +18,49 @@ def create_numbered_images(n_colors: int, output_dir: Path):
 
     square_length = 200
 
-    image_width = square_length
-    image_height = square_length
-
-
     colors = [gen_random_color() for i in range(n_colors)]
     # Draw the color
     for color_index in range(n_colors):
+        if vary_widths:
+            image_width = random.randint(200 - 50, 200 + 50)
+        else:
+            image_width = square_length
+
+        if vary_heights:
+            image_height = random.randint(200 - 50, 200 + 50)
+        else:
+            image_height = square_length
+
         image = Image.new(mode="RGB", size=(image_width, image_height))
         draw = ImageDraw.Draw(image)
 
         color_coordinates = [(0, 0),
-                             (square_length, square_length)]
+                             (image_width, image_height)]
         draw.rectangle(color_coordinates, colors[color_index])
 
         color_text = f"{color_index}"
-
         color_text_width = text_font.getlength(color_text)
         color_text_height = text_font.getmask(color_text).getbbox()[3] + descent
 
         text_coordinates = [
-            int((square_length - color_text_width) / 2),
-            int(((square_length - color_text_height) / 2)),
+            int((image_width - color_text_width) / 2),
+            int((image_height - color_text_height) / 2),
         ]
 
         draw.text(text_coordinates, color_text, (0, 0, 0), font_size = "10px", font=text_font)
         image.save(output_dir / f"{color_index}.jpg")
-        print(f"Saved {output_dir / str(color_index)}.jpg")
+        print(f"Saved {output_dir / str(color_index)}.jpg | ({image_width} x {image_height}) | ({text_coordinates})")
 
 def create_argparser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_images", default=20, type=int, help="How many square images with numbers to generate")
     parser.add_argument("--output-dir", type=Path, help="Directory where the output image will be written to",
                         default=Path("/tmp/"))
+    parser.add_argument("--vary-widths", type=bool, default=False, help="When TRUE, generated images will have varying widths")
+    parser.add_argument("--vary-heights", type=bool, default=False, help="When TRUE, generated images will have varying heights")
 
-    parser.set_defaults(func=lambda args: create_numbered_images(args.n_images, args.output_dir))
+
+    parser.set_defaults(func=lambda args: create_numbered_images(args.n_images, args.output_dir, args.vary_widths, args.vary_heights))
 
     return parser
 
